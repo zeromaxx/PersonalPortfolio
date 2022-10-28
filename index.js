d4dceaad66d1c9c5cc2c9fb94a1c3e5bdf152bf6
Aris Lamprinidis <zeromixer2010@yahoo.com>
2022-10-28 08:45:45 +0300

    added modal

--- index.js
+++ index.js
@@ -52,3 +52,48 @@ sidebar.addEventListener("click", function (e) {
 menuIcon.addEventListener("click", () => {
   sidebar.classList.toggle("toggleSidebar");
 });
+
+// Modal
+
+const modal_overlay = document.querySelector(".modal-overlay");
+const btn = document.querySelector(".modal-btn");
+const closebtn = document.querySelector(".close-btn");
+btn.addEventListener("click", function (e) {
+  modal_overlay.classList.toggle("open-modal");
+});
+closebtn.addEventListener("click", function () {
+  modal_overlay.classList.remove("open-modal");
+});
+
+// slideshow
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
