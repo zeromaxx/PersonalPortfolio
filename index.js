1ab2f449cbe178fb1442b37aa4323b9e9d6cc590
Aris Lamprinidis <zeromixer2010@yahoo.com>
2022-11-01 23:39:03 +0200

    added project 2

--- index.js
+++ index.js
@@ -31,15 +31,8 @@ hireBtn.addEventListener("click", (e) => {
 
 sidebar.addEventListener("click", function (e) {
   e.preventDefault();
-
   const clicked = e.target.closest(".links");
-  if (!clicked) return;
-  scrollLinks.forEach((c) => c.classList.remove("active-link"));
   const id = clicked.getAttribute("href").slice(1);
-
-  if (clicked) {
-    clicked.classList.add("active-link");
-  }
   const element = document.getElementById(id);
   let position = element.offsetTop;
 
@@ -53,19 +46,29 @@ menuIcon.addEventListener("click", () => {
   sidebar.classList.toggle("toggleSidebar");
 });
 
-// Modal
+// Modal 1
 
 const modal_overlay = document.querySelector(".modal-overlay");
 const btn = document.querySelector(".modal-btn");
 const closebtn = document.querySelector(".close-btn");
-btn.addEventListener("click", function (e) {
+btn.addEventListener("click", function () {
   modal_overlay.classList.toggle("open-modal");
 });
 closebtn.addEventListener("click", function () {
   modal_overlay.classList.remove("open-modal");
 });
 
-// slideshow
+// Modal 2
+const modal_overlay2 = document.querySelector(".modal-overlay-2");
+const btn2 = document.querySelector(".modal-btn-2");
+const closebtn2 = document.querySelector(".close-btn-2");
+btn2.addEventListener("click", function () {
+  modal_overlay2.classList.toggle("open-modal");
+});
+closebtn2.addEventListener("click", function () {
+  modal_overlay2.classList.remove("open-modal");
+});
+// slideshow 1
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -92,8 +95,68 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" activeDot", "");
   }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].className += " activeDot";
 }
+// slideshow 2
+
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides1(n) {
+  showSlides1((slideIndex1 += n));
+}
+
+function currentSlide1(n) {
+  showSlides1((slideIndex1 = n));
+}
+
+function showSlides1(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides-1");
+  let dots = document.getElementsByClassName("dot-1");
+  if (n > slides.length) {
+    slideIndex1 = 1;
+  }
+  if (n < 1) {
+    slideIndex1 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activeDot", "");
+  }
+  slides[slideIndex1 - 1].style.display = "block";
+  dots[slideIndex1 - 1].className += " activeDot";
+}
+
+
+let section = document.querySelectorAll("section");
+let lists = document.querySelectorAll(".list");
+function activeLink(li) {
+  lists.forEach((item) => item.classList.remove("active"));
+  li.classList.add("active");
+}
+lists.forEach((item) =>
+  item.addEventListener("click", function () {
+    activeLink(this);
+  })
+);
+
+// Add active class on Scroll
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      const target = document.querySelector(`[href='#${id}']`).parentElement;
+      activeLink(target);
+    }
+  });
+};
